68c3f6b171ed33a33794a375cad2774dae45690f
LekhrajK <lrlucky6042@gmail.com>
2020-07-21 09:59:36 +0530

    Updates

---
+++ precache-manifest.0ba5356ffacfcbf3c57cd5b231a7705e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbea58e94c95da7baafaccba5d746a4e",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "295c59bd5e56a4a62291",
+    "url": "/build/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "67f2cf2876a6d34205f1",
+    "url": "/build/static/css/main.0b80e056.chunk.css"
+  },
+  {
+    "revision": "295c59bd5e56a4a62291",
+    "url": "/build/static/js/2.cb11cea2.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/build/static/js/2.cb11cea2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f2cf2876a6d34205f1",
+    "url": "/build/static/js/main.86e35e5f.chunk.js"
+  },
+  {
+    "revision": "427a53e9f8da530b44c2",
+    "url": "/build/static/js/runtime-main.9bdef3b5.js"
+  },
+  {
+    "revision": "0965d2ae60f37621cf9fb98373887cb2",
+    "url": "/build/static/media/add.0965d2ae.svg"
+  },
+  {
+    "revision": "788b107c769a8f26ea97ffd72afaf70f",
+    "url": "/build/static/media/edit.788b107c.svg"
+  },
+  {
+    "revision": "d49022225b088ffe67edd46b7c31191b",
+    "url": "/build/static/media/empty.d4902222.svg"
+  },
+  {
+    "revision": "e1473c2c3a73a6e9f43985e1ae04953b",
+    "url": "/build/static/media/error.e1473c2c.svg"
+  }
+]);
