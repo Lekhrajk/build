3dab0186e7f4a158d6ed7b597b528c216d545c5d
LekhrajK <lrlucky6042@gmail.com>
2020-07-22 12:04:02 +0530

    complete project

--- src/components/layout/View.jsx
+++ src/components/layout/View.jsx
@@ -3,17 +3,16 @@ import { useRecoilValue } from "recoil";
 import { viewState } from "../../recoil/productRecoil";
 
 const ColumnView = (props) => {
-  return <div className="col-md-3 col-12 text-center">{props.children}</div>;
+    return <div className="col-md-3 col-12 text-center">{props.children}</div>;
 };
 
 const RowView = (props) => {
-  return <div className="col-md-6 col-12 text-center">{props.children}</div>;
+    return <div className="col-md-6 col-12 text-center">{props.children}</div>;
 };
 
 const View = (props) => {
-  const view = useRecoilValue(viewState);
-  if (view === "column") return <ColumnView>{props.children}</ColumnView>;
-  else return <RowView>{props.children}</RowView>;
+    const view = useRecoilValue(viewState);
+    if (view === "column") return <ColumnView>{props.children}</ColumnView>;
+    else return <RowView>{props.children}</RowView>;
 };
-
 export default View;
